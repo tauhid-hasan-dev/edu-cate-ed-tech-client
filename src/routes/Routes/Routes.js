0a43df0bf1cbc6b2a97b7d0003efbb5925b7b835
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-24 21:15:24 +0300

    routing set up completed for navbar menus

--- src/routes/Routes/Routes.js
+++ src/routes/Routes/Routes.js
@@ -1,10 +1,32 @@
 import { createBrowserRouter } from "react-router-dom";
+import Blog from "../../components/Blog/Blog";
+import Courses from "../../components/Courses/Courses";
+import Faq from "../../components/Faq/Faq";
+import Home from "../../components/Home/Home";
 import Main from "../../layouts/Main/Main";
 
 
 export const router = createBrowserRouter([
     {
         path: '/',
-        element: <Main></Main>
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/home',
+                element: <Home></Home>
+            },
+            {
+                path: '/courses',
+                element: <Courses></Courses>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            },
+            {
+                path: '/faq',
+                element: <Faq></Faq>
+            }
+        ]
     }
 ])
