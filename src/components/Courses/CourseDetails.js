a9e4d9ba704b317c11df5a2eb5460d4215f36d19
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-25 21:04:36 +0300

    details page bar completed

--- src/components/Courses/CourseDetails.js
+++ src/components/Courses/CourseDetails.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaFileDownload } from 'react-icons/fa';
+import { FaFileDownload, FaStar, FaStarHalfAlt } from 'react-icons/fa';
 import { useLoaderData } from 'react-router-dom';
 
 const CourseDetails = () => {
@@ -18,14 +18,44 @@ const CourseDetails = () => {
                 </div>
             </div>
             <button className="bg-btn-color bg-gray-900 text-black py-5 px-4  rounded-b  bottom-0 w-full flex justify-between items-center gap-3">
-                <div className='flex  items-center justify-between gap-3 border-r pr-5 border-slate-600'>
-                    <img className=' w-14 h-14 text-center rounded-full border-4 border-white ' src={instructor?.img} alt="" />
-                    <div className='flex flex-col text-start'>
-                        <p className=' text-slate-500'>Instructor</p>
-                        <p className='text-slate-200'>{instructor.name}</p>
+                <div className='flex gap-5'>
+                    <div className='flex  items-center justify-between gap-3 border-r pr-5 border-slate-600'>
+                        <img className=' w-14 h-14 text-center rounded-full border-4 border-white ' src={instructor?.img} alt="" />
+                        <div className='flex flex-col text-start'>
+                            <p className=' text-slate-500'>Instructor</p>
+                            <p className='text-slate-200'>{instructor.name}</p>
+                        </div>
+                    </div>
+                    {/* <div className='flex  items-center justify-between gap-3 border-r pr-5 border-slate-600'>
+                        <div className='flex flex-col text-start gap-1'>
+                            <div className='flex gap-1 text-slate-400 text-lg'>
+                                <FaStar></FaStar>
+                                <FaStar></FaStar>
+                                <FaStar></FaStar>
+                                <FaStar></FaStar>
+                                <FaStarHalfAlt></FaStarHalfAlt>
+                            </div>
+                            <p className='text-slate-200'>Review: {rating.number}</p>
+                        </div>
+                    </div> */}
+                    <div className='flex  items-center justify-between gap-3 border-r pr-5 border-slate-600'>
+                        <div className='flex flex-col text-start'>
+                            <p className=' text-slate-500'>Review</p>
+                            <div className='flex items-center gap-2'>
+
+                                <p className='text-slate-200'>{rating.number}</p>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className='flex  items-center justify-between gap-3 border-r pr-5 border-slate-600'>
+                        <div className='flex flex-col text-start'>
+                            <p className=' text-slate-500'>Students</p>
+                            <p className='text-slate-200'>{total_student}</p>
+                        </div>
                     </div>
                 </div>
-                <div className='text-red-400 text-3xl font-semibold'>
+                <div className='text-green-400 text-3xl font-semibold'>
                     <p>{price}</p>
                 </div>
             </button>
