08b34a3f63ce1d11ba45d19a064cfa235ed4f1df
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-27 00:03:14 +0300

    env local file added any key hidden now

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAPR3c9oeCuqM2pzYHrVu-IT4fi44HzRBo",
-    authDomain: "learning-platform-client-d552c.firebaseapp.com",
-    projectId: "learning-platform-client-d552c",
-    storageBucket: "learning-platform-client-d552c.appspot.com",
-    messagingSenderId: "331271002804",
-    appId: "1:331271002804:web:7e53b463683c45435655e1"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
