81e6b3e44580b8865cce464a3800d539cf0d5242
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-26 21:11:36 +0300

    react to pdf added to the details page

--- src/components/Courses/CourseDetails.js
+++ src/components/Courses/CourseDetails.js
@@ -1,6 +1,9 @@
-import React from 'react';
+
 import { FaFileDownload, } from 'react-icons/fa';
 import { Link, useLoaderData } from 'react-router-dom';
+import React, { createRef } from "react";
+import Pdf from "react-to-pdf";
+const ref = createRef();
 
 const CourseDetails = () => {
     const singleCourse = useLoaderData();
@@ -9,14 +12,26 @@ const CourseDetails = () => {
 
     return (
         <div className="cursor-pointer card rounded-md card-compact bg-gray-800  border-green-500 items-center ">
-            <div className="card-body items-center text-center m-5 ">
+            <div className="card-body items-center text-center m-5 border-b border-slate-600">
                 <div className='flex items-center gap-5  lg:gap-10'>
                     <h2 className="card-title text-white text-2xl lg:text-4xl">{title}</h2>
-                    <div className='text-white text-3xl'>
-                        <FaFileDownload></FaFileDownload>
-                    </div>
+                    <Pdf targetRef={ref} filename="code-example.pdf">
+                        {({ toPdf }) => <button onClick={toPdf}> <div className='text-white text-3xl'>
+                            <FaFileDownload></FaFileDownload>
+                        </div></button>}
+                    </Pdf>
                 </div>
             </div>
+            <div ref={ref} className='text-slate-300 pb-5 text-center '>
+                <p className='font-bold text-white'>Course Summery</p>
+                <p>Course Name: <span className='text-green-500'>{title}</span></p>
+                <p>Course Instructor: <span className='text-green-500'>{instructor.name}</span></p>
+                <p>Course Rating: <span className='text-green-500'>{rating.number}</span></p>
+                <p>Total Student: <span className='text-green-500'>{total_student}</span></p>
+                <p>Course Price: <span className='text-green-500'>{price}</span></p>
+            </div>
+
+
             <button className="bg-btn-color bg-gray-900 text-black py-5 px-4  rounded-b  bottom-0 w-full flex justify-between items-center gap-3">
                 <div className='flex lg:flex-row flex-col gap-5'>
                     <div className='flex  items-center justify-between gap-3 border-r pr-5 border-slate-600'>
@@ -53,6 +68,8 @@ const CourseDetails = () => {
                 </button>
                 <p className='text-md lg:text-lg py-4 text-slate-300'>{details}</p>
             </div>
+
+
             <div className="bg-btn-color bg-gray-900 text-black py-3 px-4 rounded-b  bottom-0 w-full flex justify-between items-center  gap-3">
                 <div className='text-green-400 text-3xl font-semibold'>
                     <p>{price}</p>
