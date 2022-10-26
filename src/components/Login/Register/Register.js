53bf3041bc9bd8327dd705096a43b2791f66a1a8
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-26 12:24:06 +0300

    getting user details to create user

--- src/components/Login/Register/Register.js
+++ src/components/Login/Register/Register.js
@@ -1,35 +1,51 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { FaGithub, FaGoogle } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthProvider';
+
 
 const Register = () => {
+
+    const { createUser } = useContext(AuthContext);
+    console.log(createUser);
+
+    const handleSignUp = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const photoURL = form.photourl.value;
+        const password = form.password.value;
+        console.log(name, email, photoURL, password);
+    }
+
     return (
         <div className='px-5 lg:px-20  py-10  flex flex-col items-center text-slate-300 bg-gray-800'>
-            <form className="p-7 lg:p-10  border rounded border-green-300 w-[350px]   lg:w-[450px] " >
+            <form onSubmit={handleSignUp} className="p-7 lg:p-10  border rounded border-green-300 w-[350px]   lg:w-[450px] " >
                 <p className='text-center text-2xl  font-semibold'>Register</p>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text text-slate-300">Full Name</span>
                     </label>
-                    <input type="text" name='name' placeholder="Your Full Name" className="input input-bordered " required />
+                    <input type="text" name='name' placeholder="Your Full Name" className="input input-bordered text-green-400 font-semibold bg-gray-700" required />
                 </div>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text text-slate-300">Photo Url</span>
                     </label>
-                    <input type="text" name='photourl' placeholder="Your Photo Url" className="input input-bordered " required />
+                    <input type="text" name='photourl' placeholder="Your Photo Url" className="input input-bordered text-green-400 font-semibold bg-gray-700" required />
                 </div>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text text-slate-300">Email</span>
                     </label>
-                    <input type="email" name='email' placeholder="Your Valid Email" className="input input-bordered " required />
+                    <input type="email" name='email' placeholder="Your Valid Email" className="input input-bordered text-green-400 font-semibold bg-gray-700" required />
                 </div>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text text-slate-300">Password</span>
                     </label>
-                    <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                    <input type="password" name='password' placeholder="Password" className="input input-bordered text-green-400 font-semibold bg-gray-700" required />
 
                 </div>
                 <div className='text-red-600 bg-red-100 mb-3 mt-2 '>
@@ -42,7 +58,7 @@ const Register = () => {
                     </>}</label>
                 </div>
                 <div className="form-control mt-6">
-                    <button className="btn  text-black border-btn-color hover:bg-green-500 hover:border-green-500 bg-green-400 border-green-400">Login</button>
+                    <button type='submit' className="btn  text-black border-btn-color hover:bg-green-500 hover:border-green-500 bg-green-400 border-green-400">Login</button>
                 </div>
                 <div className="text-center">
                     <small className="mr-2">Already have an account?</small>
--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,11 +1,20 @@
 import React, { createContext } from 'react';
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import app from '../firebase/firebase.config';
 
+const auth = getAuth(app);
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
 
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
     const user = { displayName: "Tauhid hasan" }
-    const value = { user }
+
+
+
+    const value = { user, createUser }
 
     return (
         <AuthContext.Provider value={value} >
